ff0831865c9c633ad8326f545d693d54ae5c0718
vmanriquec <pool_rodrigo1@yahoo.com>
2013-08-31 11:57:50 -0500

    Update conexionmioa.vb

--- abacusv1/abacusData/conexionmioa.vb
+++ abacusv1/abacusData/conexionmioa.vb
@@ -1,4 +1,5 @@
 ﻿Module conexionmioa
     Public cox As String = "Data Source=USUARIO\SQLEXPRESS;Initial Catalog=abacus;Integrated Security=True"
     Public siempreid As Integer
+    public ddd as integer
 End Module
